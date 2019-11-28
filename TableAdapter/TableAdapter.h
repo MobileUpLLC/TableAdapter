ca433caff71ec2657ebef67d42cd4147c871fa0c
Nikolai Timonin <nikki.timonin@yandex.ru>
2019-11-28 13:40:06 +0300

    Initial commit

---
+++ TableAdapter/TableAdapter.h
@@ -0,0 +1,19 @@
+//
+//  TableAdapter.h
+//  TableAdapter
+//
+//  Created by Nikolai Timonin on 28.11.2019.
+//  Copyright © 2019 MobileUp LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TableAdapter.
+FOUNDATION_EXPORT double TableAdapterVersionNumber;
+
+//! Project version string for TableAdapter.
+FOUNDATION_EXPORT const unsigned char TableAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TableAdapter/PublicHeader.h>
+
+
