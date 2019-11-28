8dc6e87cf3c47530e7eb0c91a455caa536930be0
Nikolai Timonin <nikki.timonin@yandex.ru>
2019-11-28 14:46:13 +0300

    Add example projet

---
+++ Example/TableAdapterExample/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  TableAdapterExample
+//
+//  Created by Nikolai Timonin on 28.11.2019.
+//  Copyright © 2019 MobileUp LLC. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ Example/TableAdapterExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableAdapterExample
+//
+//  Created by Nikolai Timonin on 28.11.2019.
+//  Copyright © 2019 MobileUp LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
