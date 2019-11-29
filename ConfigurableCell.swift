7df73f8b3fdf6456537c140d8dd341d317139ae3
Nikolai Timonin <nikki.timonin@yandex.ru>
2019-11-29 11:59:46 +0300

    Add cell protocols

---
+++ ConfigurableCell.swift
@@ -0,0 +1,8 @@
+//
+//  ConfigurableCell.swift
+//  Pods-TableAdapterExample
+//
+//  Created by Nikolai Timonin on 29.11.2019.
+//
+
+import Foundation
---
+++ Source/TableAdapterDataSource.swift
@@ -0,0 +1,8 @@
+//
+//  TableAdapterDataSource.swift
+//  Pods
+//
+//  Created by Nikolai Timonin on 29.11.2019.
+//
+
+import Foundation
