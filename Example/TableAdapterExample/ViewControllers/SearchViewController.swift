154ae06fd8702540085dae1a39e6f2e2e4854846
Nikolai Timonin <nikki.timonin@yandex.ru>
2019-12-13 16:08:56 +0300

    Refactor search example

--- Example/TableAdapterExample/ViewControllers/SearchViewController.swift
+++ Example/TableAdapterExample/ViewControllers/SearchViewController.swift
@@ -19,22 +19,21 @@ class SearchViewController: UIViewController {
     
     private lazy var adapter = TableAdapter(tableView: tableView)
     
-    private var filter: String?
-    
     private lazy var words: [String] = {
         
-        let str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        let string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        
         
         var unique = Set<String>()
         var words = [String]()
         
-        let range = str.startIndex ..< str.endIndex
-        str.enumerateSubstrings(in: range, options: .byWords) { (substring, _, _, _) in
-            guard let substring = substring else { return }
-            if !unique.contains(substring) {
-                unique.insert(substring)
-                words.append(substring)
-            }
+        string.enumerateSubstrings(in: string.startIndex..<string.endIndex, options: .byWords) { (word, _, _, _) in
+            
+            guard let word = word, unique.contains(word) == false else { return }
+            
+            unique.insert(word)
+            
+            words.append(word)
         }
         
         return words
@@ -72,12 +71,11 @@ class SearchViewController: UIViewController {
     private func setupSearchController() {
         
         searchController.searchResultsUpdater = self
+        navigationItem.hidesSearchBarWhenScrolling = false
         searchController.obscuresBackgroundDuringPresentation = false
         searchController.searchBar.placeholder = "Search"
+        
         navigationItem.searchController = searchController
-        definesPresentationContext = true
-        searchController.isActive = true
-        navigationItem.hidesSearchBarWhenScrolling = false
     }
 }
 
@@ -87,15 +85,13 @@ extension SearchViewController: UISearchResultsUpdating {
     
     func updateSearchResults(for searchController: UISearchController) {
         
-        filter = searchController.searchBar.text
-        
-        guard let filter = filter, filter.isEmpty == false else {
+        guard let filter = searchController.searchBar.text, filter.isEmpty == false else {
             
             return adapter.update(with: words, animated: true)
         }
         
-        let akk = words.filter { $0.lowercased().contains(filter.lowercased()) }
+        let filteredWords = words.filter { $0.lowercased().contains(filter.lowercased()) }
         
-        adapter.update(with: akk, animated: true)
+        adapter.update(with: filteredWords, animated: true)
     }
 }
