b767063cf7e124322fafa956e92f7a140f69e555
Nikolai Timonin <nikki.timonin@yandex.ru>
2020-05-14 10:54:41 +0300

    Align parameters in app delegate

--- Example/TableAdapterExample/AppDelegate.swift
+++ Example/TableAdapterExample/AppDelegate.swift
@@ -14,8 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+        _ application                               : UIApplication,
+        didFinishLaunchingWithOptions launchOptions : [UIApplication.LaunchOptionsKey : Any]?
     ) -> Bool {
         
         return true
